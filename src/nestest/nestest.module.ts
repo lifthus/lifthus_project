e4763de9c43e31607ce8a4586cdd3056dcbd117b
lifthus <lifthus531@gmail.com>
2022-11-08 12:48:49 +0900

    Module finished

--- src/nestest/nestest.module.ts
+++ src/nestest/nestest.module.ts
@@ -1,9 +1,35 @@
-import { Module } from '@nestjs/common';
+import { Module, Global, DynamicModule } from '@nestjs/common';
 import { SBDController } from './nestest.controller';
 import { SBDService } from './nestest.service';
 
+// with @Global(), can make the class as glolbal but not suggested.
 @Module({
     controllers: [ SBDController ],
-    providers: [ SBDService ]
+    providers: [ SBDService ],
+    exports: [ SBDService ]
 })
-export class SBDModule{}
+export class SBDModule{
+    // and also  a module class can inject providers. (for config purpose)
+    constructor( private sbdService: SBDService){}
+    // but module itself can't be injected as provider due to 
+}
+
+@Module({
+    providers:[/* Conn */],
+})
+export class DyMdTestModule {
+    static forRoot(entities=[], options?): DynamicModule {
+        const providers = {/* createblahProviders(options, entities); */}
+        return {
+            //global: True,
+            module: DyMdTestModule,
+            //providers: providers,
+           //exports:providers
+        }; // foRoot method may return a dynamic module either synchronously or async(via a Promise)
+    }
+// this module defines the Conn by default but depending on entities and options,
+// it exposes a colleciton of them.
+// Note that properties returned by the dynamic module 'extend' the base module.
+// and it can be imported later like impoorts: [DyMdTestModule.forRoot([User])],
+// it want to re-export it, can omit the forRoot() like
+// exports:[DyMdTestModule],
